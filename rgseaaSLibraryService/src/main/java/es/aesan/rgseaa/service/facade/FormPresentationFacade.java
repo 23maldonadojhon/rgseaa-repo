0fc49ab8866fe9a4eaf8ee9d80d0d4b3d3afc01d
Luis Mora <morareylu@gmail.com>
2024-08-19 21:07:02 -0400

    actualizando  formpresentation

--- rgseaaSLibraryService/src/main/java/es/aesan/rgseaa/service/facade/FormPresentationFacade.java
+++ rgseaaSLibraryService/src/main/java/es/aesan/rgseaa/service/facade/FormPresentationFacade.java
@@ -9,6 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class FormPresentationFacade extends AbstractFacade<
@@ -26,9 +30,29 @@ public class FormPresentationFacade extends AbstractFacade<
     }
 
     @Override
+    public void  update(FormPresentationDto dto){
+       FormPresentation formPresentation= formPresentationConverter.dtoToEntity(dto);
+       formPresentationService.update(formPresentation);
+    }
+
+    @Override
+    public List<FormPresentationDto> list(GeneralCriteria criteria){
+        Collection<FormPresentation> formPresentations= formPresentationService.list(criteria);
+        List<FormPresentationDto> formPresentationDtoList=formPresentationConverter.mapEntityToDtoList(new ArrayList<>(formPresentations));
+        return formPresentationDtoList;
+    }
+
+    @Override
     public Page<FormPresentationDto> page(GeneralCriteria criteria){
         Page<FormPresentation> page= formPresentationService.page(criteria);
         Page<FormPresentationDto> dtoPage=formPresentationConverter.mapEntityToDtoPage(page);
         return dtoPage;
+    }
+
+    @Override
+    public FormPresentationDto get(Long id){
+       FormPresentation formPresentation= formPresentationService.get(id);
+       FormPresentationDto formPresentationDto= formPresentationConverter.entityToDto(formPresentationService.get(id));
+       return formPresentationDto;
     }
 }
