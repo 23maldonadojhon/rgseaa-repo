a1d21207f04f019a3365165fd5f7ab5522bfc0d5
Luis Mora <morareylu@gmail.com>
2024-09-19 17:10:18 -0400

    UPDATE USER

--- rgseaaSLibraryDomain/src/main/java/es/aesan/rgseaa/model/entity/User.java
+++ rgseaaSLibraryDomain/src/main/java/es/aesan/rgseaa/model/entity/User.java
@@ -21,6 +21,7 @@ import java.util.Set;
 public class User extends AuditedBaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
 
