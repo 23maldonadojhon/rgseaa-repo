b726686e531ce4851b6c535117c1aa90855c05e0
Luis Mora <morareylu@gmail.com>
2024-09-23 19:19:01 -0400

    working with

--- rgseaaSLibraryService/src/main/java/es/aesan/rgseaa/service/repository/CcaaRepository.java
+++ rgseaaSLibraryService/src/main/java/es/aesan/rgseaa/service/repository/CcaaRepository.java
@@ -3,6 +3,8 @@ package es.aesan.rgseaa.service.repository;
 
 import es.aesan.rgseaa.model.commom.criteria.GeneralCriteria;
 import es.aesan.rgseaa.model.entity.Ccaa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,14 @@ import java.util.List;
 public interface CcaaRepository extends BaseRepository<Ccaa,Long>,  QueryByCriteria<Ccaa, GeneralCriteria> {
 
     @Query(value = "SELECT c FROM Ccaa c")
+    Page<Ccaa> findAllByCriteria(GeneralCriteria criteria, Pageable pageable);
+
+    @Query(value="SELECT c FROM Ccaa c WHERE c.enrollment!='AND'")
+    List<Ccaa> getAllCcaaAvaliable();
+
+    @Query(value = "SELECT c FROM Ccaa c")
     List<Ccaa> findAll(GeneralCriteria criteria);
+
+
+
 }
